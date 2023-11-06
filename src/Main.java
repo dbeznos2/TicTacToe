6d1e7c0709ef8b65336851c221a82e8b17c3a677
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-11-06 16:30:52 +0100

    class structure for Tic-Tac-Toe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+import java.util.Scanner; // Import the Scanner class from the java.util package.
+
+public class Main { // Define a class named "TicTacToe."
+    public static void main(String[] args) { // The main method, the entry point of the program.
+        char[][] board = new char[3][3]; // Declare a 2D character array to represent the game board.
+        char currentPlayer = 'X'; // Initialize the current player to 'X'.
     }
 }
