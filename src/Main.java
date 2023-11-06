f2b9cbbcc569ea97419ab8952c996eb38d6c0282
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-11-06 16:35:50 +0100

    first move flag

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,17 @@ public class Main { // Define a class named "TicTacToe."
     public static void main(String[] args) { // The main method, the entry point of the program.
         char[][] board = new char[3][3]; // Declare a 2D character array to represent the game board.
         char currentPlayer = 'X'; // Initialize the current player to 'X'.
+        initializeBoard(board); // Call the initializeBoard method to set up the game board.
+        printBoard(board); // Call the printBoard method to display the initial game board.
+
+        boolean firstMove = true; // A boolean flag to track if it's the first move of the game.
+        int moves = 0; // Initialize the number of moves to 0.
+
+    }
+
+    private static void printBoard(char[][] board) {
+    }
+
+    private static void initializeBoard(char[][] board) {
     }
 }
