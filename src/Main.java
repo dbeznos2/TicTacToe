55a28de3c578c34b873de5f05c1c042b2b996240
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-11-07 08:26:50 +0100

    Printing the game board

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main { // Define a class named "TicTacToe."
         char[][] board = new char[3][3];
         char currentPlayer = 'X';
         initializeBoard(board);
+        printBoard(board);
+
     }
 
     public static void initializeBoard(char[][] board) {
@@ -15,6 +17,17 @@ public class Main { // Define a class named "TicTacToe."
             }
         }
     }
+    public static void printBoard(char[][] board) {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+
 }
 
 
