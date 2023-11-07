f72cc661937bd14e6163a914bd714d76862e7103
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-11-07 09:33:34 +0100

    Optimize getPlayerMove method to eliminate code repetition

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
-import java.util.Scanner; // Import the Scanner class from the java.util package.
+import java.util.Scanner;
 
-public class Main { // Define a class named "TicTacToe."
+public class Main {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
         char currentPlayer = 'X';
@@ -34,8 +34,8 @@ public class Main { // Define a class named "TicTacToe."
     }
 
     public static void initializeBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) { //rows
+            for (int j = 0; j < 3; j++) { //columns
                 board[i][j] = ' ';
             }
         }
@@ -55,20 +55,18 @@ public class Main { // Define a class named "TicTacToe."
     public static int getPlayerMove(char[][] board, char currentPlayer) {
         Scanner scanner = new Scanner(System.in);
         int move;
+        int row, col;
 
         while (true) {
             System.out.print("Player " + currentPlayer + ", enter your move (1-9): ");
             move = scanner.nextInt();
-            if (move >= 1 && move <= 9) {
-                int row = (move - 1) / 3;
-                int col = (move - 1) % 3;
-                if (board[row][col] == ' ') {
-                    break;
-                } else {
-                    System.out.println("Invalid move. Please choose an empty cell.");
-                }
+            row = (move - 1) / 3;
+            col = (move - 1) % 3;
+
+            if (move >= 1 && move <= 9 && board[row][col] == ' ') {
+                break;
             } else {
-                System.out.println("Invalid input. Please enter a number between 1 and 9.");
+                System.out.println("Invalid move. Please choose an empty cell.");
             }
         }
 
