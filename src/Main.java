9802cf6aa60c020a512b533380c9f1f944a0d4bb
dbeznos2 <dmytro.beznoskovych@jobtrek.ch>
2023-11-07 08:23:05 +0100

    game board initialization

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@
 import java.util.Scanner; // Import the Scanner class from the java.util package.
 
 public class Main { // Define a class named "TicTacToe."
-    public static void main(String[] args) { // The main method, the entry point of the program.
-        char[][] board = new char[3][3]; // Declare a 2D character array to represent the game board.
-        char currentPlayer = 'X'; // Initialize the current player to 'X'.
-        initializeBoard(board); // Call the initializeBoard method to set up the game board.
-        printBoard(board); // Call the printBoard method to display the initial game board.
-
-        boolean firstMove = true; // A boolean flag to track if it's the first move of the game.
-        int moves = 0; // Initialize the number of moves to 0.
-
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        char currentPlayer = 'X';
+        initializeBoard(board);
     }
 
-    private static void printBoard(char[][] board) {
-    }
-
-    private static void initializeBoard(char[][] board) {
+    public static void initializeBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
     }
 }
+
+
+
+
+
